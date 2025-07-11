a2f438fcee60e19bba5d55162c14e337f627e103
gr-sain <gouravsainittechexpert.com>
2025-07-11 14:58:05 +0530

    migrate

--- database/migrations/2025_07_10_105252_create_user_requests_create.php
+++ database/migrations/2025_07_10_105252_create_user_requests_create.php
@@ -6,9 +6,9 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
     public function up(): void {
-        Schema::create('user_requests', function (Blueprint $table) {
+        Schema::create('usdt_requests', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained('user_usdts')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('address_id')->constrained('addresses')->onDelete('cascade');
             $table->string('status')->default('pending');
             $table->timestamps();
