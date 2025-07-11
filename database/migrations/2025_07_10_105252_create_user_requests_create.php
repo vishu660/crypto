74664413f0067d69998f687786dbe68376887318
gr-sain <gouravsainittechexpert.com>
2025-07-11 14:46:37 +0530

    change name

--- database/migrations/2025_07_10_105252_create_user_requests_create.php
+++ database/migrations/2025_07_10_105252_create_user_requests_create.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
     public function up(): void {
-        Schema::create('usdt_requests', function (Blueprint $table) {
+        Schema::create('user_requests', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('user_usdts')->onDelete('cascade');
             $table->foreignId('address_id')->constrained('addresses')->onDelete('cascade');
